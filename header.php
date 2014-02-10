9ee26116dd7509b6c62ecef25d66c3736471adda
jcyl29 <justincylui@gmail.com>
2014-02-09 20:36:03 -0800

    first attempt at mobile theme

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
@@ -39,15 +39,40 @@
 </script>
 </head>
 <body>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
-<script src="<?php bloginfo('template_url'); ?>/js/jquery.example.min.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js" type="text/javascript"></script>
 <script type="text/javascript" >
 	$(document).ready(function() {
-		$('#s').example('Type search words, press Enter');
+		// $('#s').example('Type search words, press Enter');
 	});
 </script>
+<input type="checkbox" class="main-nav-check" id="main-nav-check" />
+<nav class="main-nav" id="main-nav">
+    <a href="<?php echo get_permalink(8); ?>" title="Home"><?php echo get_the_title(8) ?></a>
+    <a href="<?php echo get_permalink(7); ?>" title="About H324"><?php echo get_the_title(7) ?></a>
+    <a href="<?php echo get_permalink(9); ?>" title="Testimonials"><?php echo get_the_title(9) ?></a>
+    <a href="<?php echo get_permalink(152); ?>" title="Blog"><?php echo get_the_title(152) ?></a>
+    <a href="<?php echo get_permalink(10); ?>" title="Contact Us"><?php echo get_the_title(10); ?></a>
+
+    <form method="post" action="http://www.aweber.com/scripts/addlead.pl" id="signup_form">
+        <input type="hidden" name="meta_web_form_id" value="243814305" />
+        <input type="hidden" name="meta_split_id" value="" />
+        <input type="hidden" name="listname" value="h324" />
+        <input type="hidden" name="redirect" value="http://www.aweber.com/thankyou-coi.htm?m=video" id="redirect_dc8f5d86a22c0ab1d2aadf45c83b4ceb" />
+        <input type="hidden" name="meta_adtracking" value="H324header" />
+        <input type="hidden" name="meta_message" value="1" />
+        <input type="hidden" name="meta_required" value="email" />
+        <input type="hidden" name="meta_forward_vars" value="" />
+        <input type="hidden" name="meta_tooltip" value="" />
+        <h2>Get a Free  Presentation Tip Every Tuesday!</h2>
+        <p>Upgrade Your Skills Weekly</p>
+        <input type="text" name="email" size="22" id="awf_field-6768882" value="Enter your email here" />
+        <div style="display: none;"><img src="http://forms.aweber.com/form/displays.htm?id=TCzMHIwszAys" alt="" /></div>
+    </form> 
+
+</nav>
 <div id="wrapper">
 	<div id="header">
+        <label for="main-nav-check" class="toggle-menu">☰</label>
         <a href="<?php bloginfo('url');?>" style="display: inline-block; height: 81px; margin: 10px 0 0 26px; width: 414px;"></a>
         <ul id="banner_links">
 	        <li><a href="<?php echo get_permalink(8); ?>" title="Home"><?php echo get_the_title(8) ?></a></li>
